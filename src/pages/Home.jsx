cda7a1b9fb20055e95f8252333b1abefae8fdfb6
eduardo-solis <solishernandezeduardosolis@gmail.com>
2022-11-11 16:24:05 -0600

    Paso 5: Enlistando los elementso necesarios para la pagina del HOme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+        <p>Elementos de la paguina de inicio</p>
+        <ul>
+            <li>Barra de navegacion</li>
+            <li>Logos</li>
+            <li>Informacion sobre el congreso</li>
+            <li>Boton de redireccionamiento</li>
+        </ul>
+        </>
     )
 }
 
