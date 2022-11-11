a00ca2fb554a084ffb3df0a529c35c9c954a90d5
eduardo-solis <solishernandezeduardosolis@gmail.com>
2022-11-11 16:18:21 -0600

    Paso 3: Creando las rutas del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' />
+      <Route path='/participantes' element={ <h1>Participantes</h1> } />
+      <Route path='/participantes/registrar' element={ <h1>Registrar Participante</h1> } />
+      <Route path='/participantes/editar' element={ <h1>Editar Participante</h1> } />
+      <Route path='*' element={ <h1>Error 404: Pagina no encontrada</h1> } />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
