0b919da5edefea9c98b8cd8206c2d9a55577bf0f
eduardo-solis <solishernandezeduardosolis@gmail.com>
2022-11-12 19:45:41 -0600

    Paso 16: Creando el archivo types para el uso del reducer

---
+++ src/context/types.js
@@ -0,0 +1,9 @@
+/**
+ * Archivo que contiene la lista de acciones para
+ * usar dentro del Reducer
+ */
+
+export const GET_PARTICIPANTES = "GET_PARTICIPANTES";
+export const ADD_PARTICIPANTES = "ADD_PARTICIPANTES";
+export const MODIFY_PARTICIPANTES = "MODIFY_PARTICIPANTES";
+export const DELETE_PARTICIPANTES = "DELETE_PARTICIPANTES";
